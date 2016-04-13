949af88a127e0063f0193c792d5e75da31cf0322
Kenneth Olson <kenneth.ko.olson@gmail.com>
2016-04-12 19:24:33 -0700

    add loop counter

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,3 +1,8 @@
+puts "How many employees will be processed"
+employees = gets.to_i
+	until employees == 0
+	employees = employees - 1
+	
 puts "What is your name"
 name = gets.chomp
 
@@ -13,12 +18,15 @@ garlic = gets.chomp
 puts "Would you like to enroll in our company health insurance y/n"
 insurance = gets.chomp
 
-
-if (age == 2016 - year || age == 2015 - year) && (garlic == "y" || insurance =="y")
+if name == "Drake Cula" || name == "Tu Fang"
+		puts "definately a vampire"
+	elsif (age == 2016 - year || age == 2017 - year) && (garlic == "y" || insurance =="y")
 		puts "probably not a vampire"
+	elsif age != 2016 - year && garlic == "n" && insurance =="n"
+		puts "Almost certainly a vampire"
 	elsif age != 2016 - year && (garlic == "n" || insurance =="n")
-		puts "probably a vampire"
+		puts "probably a vampire"		
 	else
 		puts "incunclusive"
 end
-	
+end

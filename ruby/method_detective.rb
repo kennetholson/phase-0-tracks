f25f6eb16db0b6542a539811c6f4350e8679ae22
Kenneth Olson <kenneth.ko.olson@gmail.com>
2016-04-19 15:50:23 -0700

    found method suspects

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -32,8 +32,6 @@
 # => "Elementary, my dear Watson!"
 
 # "z".<???>
-# => 122 
-# (What is the significance of the number 122 in relation to the character z?)
 
 # "How many times does the letter 'a' appear in this string?".<???>
 # => 4

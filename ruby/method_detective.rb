34fdfcbde84ef8a3e3a8e1810e540c9fdc674b91
Kenneth Olson <kenneth.ko.olson@gmail.com>
2016-04-19 15:50:23 -0700

    found method suspects

--- ruby/method_detective.rb
+++ ruby/method_detective.rb
@@ -4,36 +4,39 @@
 # When there's more than one suspect who could have
 # committed the crime, add additional calls to prove it.
 
-# “iNvEsTiGaTiOn”.<???>
+“iNvEsTiGaTiOn”.swapcase
 # => “InVeStIgAtIoN”
 
-# “zom”.<???>
+“zom”.insert(1, o)
 # => “zoom”
 
-# “enhance”.<???>
+“enhance”.rjust(8).ljust(12)
+"enhance".center(10)
 #=> “    enhance    ”
 
-# “Stop! You’re under arrest!”.<???>
+“Stop! You’re under arrest!”.upcase
 # => “STOP! YOU’RE UNDER ARREST!”
 
-# “the usual”.<???>
-#=> “the usual suspects”
+“the usual”<<" suspects"
+#=> “the usual suspects"
 
-#“ suspects”.<???>
-#=> “the usual suspects”
+"suspects".insert(0, "the usual ")
+"suspects".prepend("the usual ")
+#=> "the usual suspects"
 
-# “The case of the disappearing last letter”.<???>
+“The_case of the disappearing last letter”.chop
 # => “The case of the disappearing last lette”
 
-# “The mystery of the missing first letter”.<???>
+“The mystery of the missing first letter”.insert(0, " ")
 # => “he mystery of the missing first letter”
 
-# “Elementary  ,    my   dear      Watson!”.<???>""
+“Elementary  ,    my   dear      Watson!”.squeeze
 # => “Elementary, my dear Watson!”
 
-#“z”.<???>
+“z”.ord
 # => 122 
 # (What is the significance of the number 122 in relation to the character z?)
+#gives the integer ordinal
 
-#“How many times does the letter ‘a’ appear in this string?”.<???>
+“How many times does the letter ‘a’ appear in this string?”.count "a"
 #=> 4

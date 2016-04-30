7c79ced95c29ebd284df4813ec0cb90c7f5f12f7
Kenneth Olson <kenneth.ko.olson@gmail.com>
2016-04-30 15:40:17 -0700

    make interactive, still need a loop

--- ruby/solo.rb
+++ ruby/solo.rb
@@ -2,20 +2,26 @@
 #create class shark
 
 class Shark
+	attr_reader :type, :length
+	attr_accessor :location
 
 #create at least 3 attributes type, length, location.	
+	
 	def initialize(type,length, location)
 		@type = type
 		@length = length
 		@location = location
+		puts "initialing new shark"
 	end
+
 #3 methods 1 that takes an arguement
+	
 	def eat(x)
-		puts "The #{@type} just ate #{x} fish"
+		puts "The #{@type} shark just ate #{x} fish"
 	end
 	
 	def tag
-		puts "The #{@type} has just been tagged in #{@location}"
+		puts "The #{@type} shark has just been tagged in #{@location}"
 	end
 	
 	def size
@@ -28,10 +34,20 @@ class Shark
 		end			
 	end
 
-end		
+end	
 
-shark = Shark.new("Great White", 16, "Cape Town")
+puts "Sharks name"
+	name = gets.chomp
+puts "Shark Type"
+	type = gets.chomp
+puts "How long is the shark"
+	length = gets.to_i
+puts "Sharks location"
+	location = gets.chomp
+name = Shark.new(type, length, location)
 
-shark.eat(3)
-shark.tag
-shark.size
+name.eat(rand(1..10))
+name.tag
+name.size
+#name.location = "australia"
+#puts "#{name.location}"

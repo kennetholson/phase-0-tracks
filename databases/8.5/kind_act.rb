95f3f202960f43267c6f21363185a6b9c8b2e386
Kenneth Olson <kenneth.ko.olson@gmail.com>
2016-05-15 14:32:55 -0700

    create table

--- databases/8.5/kind_act.rb
+++ databases/8.5/kind_act.rb
@@ -1,8 +1,19 @@
 # While working at DBC and coding comstantly I don't want to forget about the other things that are important to me,
 # depending which week I am in I want this program to remind me to do something nice for my wife.
 
-#require gems
+# require gems
 require 'sqlite3'
 
-#create database
+# create database
 todo = SQLite3::Database.new("act.db")
+
+# create table
+create_table = <<-table
+	CREATE TABLE action(
+	id INTEGER PRIMARY KEY,
+	what VARCHAR(255),
+	day VARCHAR(255)
+	)
+table
+
+todo.execute(create_table)
